0db92d1814edb5816a5da85b97f2747a71366048
moises1747 <moisesleal1902@gmail.com>
2016-08-18 10:11:35 -0400

    Add files via upload

---
+++ Numeros Consecutivos Pares.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	
+	int a,b,c,i,n;
+	printf("ingrese un numero");
+	scanf("%d", &n);
+for(i=0;i<n;i++){
+	c=a+b;
+	printf("\n%d\n",c);
+	b=c;
+	a=b;
+	}
+}
